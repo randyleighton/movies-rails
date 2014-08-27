aa7d659fec1a546ccd621cd7adfde3cd541e252d
Randy Leighton <student@epicodus.com>
2014-08-26 18:32:58 -0700

    initial commit

---
+++ config/application.rb
@@ -0,0 +1,16 @@
+require File.expand_path('../boot', __FILE__)
+
+# Pick the frameworks you want:
+require "active_model/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "sprockets/railtie"
+Bundler.require(*Rails.groups)
+
+module Movies
+  class Application < Rails::Application
+    config.action_controller.permit_all_parameters = true
+  end
+end
