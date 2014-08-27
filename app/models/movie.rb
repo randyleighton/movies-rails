14cfac49d586ec052b24f16b3efbdd3d3e3aa6ca
Randy Leighton <student@epicodus.com>
2014-08-26 19:37:35 -0700

    movies has and belongs to many relation and test

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,6 +3,6 @@ class Movie < ActiveRecord::Base
 validates :name, presence: true
 validates :year, presence: true
 
-has_and_belongs_to_many :movies
+has_and_belongs_to_many :categories
 
 end
--- spec/movies/movie_spec.rb
+++ spec/movies/movie_spec.rb
@@ -5,4 +5,5 @@ describe Movie do
 it { should validate_presence_of :name }
 it { should validate_presence_of :year }
 
+it { should have_and_belong_to_many :categories }
 end
