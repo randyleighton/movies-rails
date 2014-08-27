144e755b07061cc9931dff0d118c52dbafe89a33
Randy Leighton <student@epicodus.com>
2014-08-26 19:09:55 -0700

    movie verify presence of year and tests

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,6 @@
 class Movie < ActiveRecord::Base
 
 validates :name, presence: true
+validates :year, presence: true
 
 end
--- spec/movies/movie_spec.rb
+++ spec/movies/movie_spec.rb
@@ -3,5 +3,6 @@ require 'rails_helper'
 describe Movie do
 
 it { should validate_presence_of :name }
+it { should validate_presence_of :year }
 
 end
