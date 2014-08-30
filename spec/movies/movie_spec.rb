68fac547df27639fa89c85e20347c8fb9b56e61f
randy leighton <student@epicodus.com>
2014-08-29 19:14:10 -0700

    fixed tests for integer numericality and default scope

--- spec/movies/movie_spec.rb
+++ spec/movies/movie_spec.rb
@@ -2,9 +2,16 @@ require 'rails_helper'
 
 describe Movie do
 
-it { should validate_presence_of :name }
-it { should validate_presence_of :year }
-it { should validate_numericality_of(:number).only_integer }
-it { should have_and_belong_to_many :categories }
+  it { should validate_presence_of :name }
+  it { should validate_presence_of :year }
+  it { should validate_numericality_of(:year).only_integer }
+  it { should have_and_belong_to_many :categories }
+
+  it "should set the default scope to sort by name" do
+    movie1 = Movie.create({name: "Nacho Libre", year: 2005})
+    movie2 = Movie.create({name: "Oblivion", year: 2013})
+    movie3 = Movie.create({name: "Noah", year: 2014})
+    expect(Movie.all).to eq [movie1, movie3, movie2]
+  end
 
 end
